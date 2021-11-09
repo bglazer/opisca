59ea0502e44a987d11ff6a3eaf7d01f0e98d59cb
Bryan Glazer <bryan.glazer@vanderbilt.edu>
2021-11-08 22:13:08 -0600

    Loader is working

--- loader.py
+++ loader.py
@@ -1,38 +1,150 @@
 from torch_geometric.data import HeteroData
+import torch_geometric
+import torch
 
 class HeteroLoader():
-    def __init__(self, graph):
+    def __init__(self, graph, device):
         self.graph = graph
+        self.device = device
 
-    def sample(self, node_type, ids, n_steps): #, batch_size=None):
+    def sample(self, task, ids, n_steps):
+        layers = self.forward(self, ids, task, n_steps)
+        layers = self.backward(self, ids, task, layers)
+        return layers
+
+    def forward(self, ids, layers, n_steps): #, batch_size=None):
         with torch.no_grad():
-            newgraph = HeteroData()
-            sources = {node_type: ids}
+            sources = {task[1]: ids}
             all_nodes = {}
-            for step in n_steps:
+            layers = []
+            relations = self.graph._edge_store_dict
+
+            for step in range(n_steps):
+                # TODO is this the right place to reinit visited?
+                visited = {relation:set() for relation in relations}
+                print('Step', step)
+                print('='*80)
                 next_sources = {}
-                for source, ids in sources:
-                    for relation, edges in graph._edge_stores_dict.items():
-                        source, _, target = relation
-                        if source == node_type and relation not in newgraph:
+                edge_masks = {}
+                print('sources', sources.keys())
+                for source, next_ids in sources.items():
+                    print('----')
+                    print('source', source)
+                    for relation, edges in relations.items():
+                        next_source, _, dst = relation
+
+                        if step == n_steps - 1:
+                            if dst != task[0]:
+                                print('skipped', relation)
+                                continue
+
+                        print('relation', relation)
+                        if next_source == source:
+                            print('match', source, next_source)
                             edge_index = edges.edge_index
-                            for idx in ids:
-                                neighbors = edge_index[:,(edge_index[0]==idx).flatten()]
-                                newgraph[relation].edge_index = neighbors
+                            for idx in next_ids:
+                                if idx not in visited[relation]:
+                                    visited[relation].add(idx)
+                                    match_mask = (edge_index[0]==idx).flatten()
+                                    matching_edges = edge_index[:,match_mask]
+                                    self.update_mask(edge_masks, relation, match_mask)
+                                    self.append_sources(next_sources, dst, matching_edges[1])
 
-                            if target not in all_nodes:
-                                all_nodes[target] = neighbors[1].unique()
-                            else:
-                                all_nodes[target] = torch.cat(all_nodes[target], neighbors[1].unique(), axis=0).unique()
+                new_graph = HeteroData()
+                for relation in edge_masks:
+                    mask = edge_masks[relation]
+                    new_graph[relation].edge_index = self.graph[relation].edge_index[:,mask]
 
-                            if target not in next_sources:
-                                next_sources[target] = neighbors[1].unique()
-                            else:
-                                next_sources[target] = torch.cat(next_sources[target], neighbors[1].unique(), axis=0).unique()
+                layers.append(new_graph)
+                sources = next_sources
+                for src in sources:
+                    sources[src] = sources[src].unique()
+
+                print('next sources', next_sources.keys())
+
+        return layers
+        
+    # TODO visited
+    # TODO associate data
+    def backward(self, ids, task, layers):
+        source, target = task
+        new_layers = []
+        step = len(layers)-1
+        with torch.no_grad():
+            sources = {}
+            while step >= 0:
+                print('Step', step)
+                print('='*80)
+                edge_masks = {}
+                layer = layers[step]
+                next_sources = {}
+                relations = layer._edge_store_dict
+                for relation, edges in relations.items():
+                    edge_index = edges.edge_index
+                    print('relation', relation)
+                    src, _, dst = relation
+                    if dst == source:
+                        print('data match, source=', source, dst)
+                        full_mask = torch.ones_like(edge_index[0], dtype=bool, device=self.device)
+                        self.update_mask(edge_masks, relation, full_mask)
+                        self.append_sources(next_sources, src, edge_index[0])
+                    if dst in sources:
+                        print('source match, dst=', dst, list(sources.keys()))
+                        ids = sources[dst]
+                        for idx in ids:
+                            match_mask = (edge_index[1]==idx).flatten()
+                            matching_edges = edge_index[:,match_mask]
+                            neighbors = matching_edges[0].unique()
+                            self.update_mask(edge_masks, relation, match_mask)
+                            self.append_sources(next_sources, src, matching_edges[0])
+
+                new_graph = HeteroData()
+                for relation in edge_masks:
+                    mask = edge_masks[relation]
+                    edge_index = relations[relation].edge_index
+                    relation, edge_index = self.reverse(relation, edge_index)
+                    new_graph[relation].edge_index = edge_index[:,mask]
+
+                new_layers.append(new_graph)
                 sources = next_sources
 
-            # TODO load data, make association from subgraph to full graph indexes
-            for node_name, nodes in all_nodes.items():
-                newgraph[node_name].x = 
-                for relation, edges in newgraph._edge_stores_dict.items():
-                    newgraph[relation]
+                for src in sources:
+                    sources[src] = sources[src].unique()
+                
+                print('next sources', next_sources.keys())
+                step -= 1 
+
+        return new_layers
+
+    def reverse(self, relation, edge_index):
+        src, type, dst = relation
+        if type.startswith('rev'):
+            type = type[4:]
+        else:
+            type = 'rev_' + type
+
+        new_relation = (dst, type, src)
+
+        # switch rows in-place
+        new_edge_index = edge_index.clone()
+        new_edge_index = new_edge_index[[1,0]]
+
+        return new_relation, new_edge_index
+
+    def update_mask(self, edge_masks, relation, edges):
+        if len(edges) == 0:
+            return 
+
+        if relation not in edge_masks:
+            edge_masks[relation] = edges
+            return
+
+        edge_masks[relation] += edges
+
+    def append_sources(self, next_sources, src, neighbors):
+        if len(neighbors) == 0:
+            return
+
+        if src not in next_sources:
+            next_sources[src] = torch.empty((0,), device=self.device, dtype=int)
+        next_sources[src] = torch.cat([next_sources[src], neighbors])
